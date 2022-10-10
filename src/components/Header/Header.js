20279d68d55d51d546267bfc794c6835b1f33647
nusrat-git <nusratjahanmithila66@gmail.com>
2022-10-10 23:36:34 +0600

    header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='header-container'>
+            <div className='text-start position-absolute top-50 ps-5'>
+                <h1 className='text-white pb-4'>Test Yourself</h1>
+                <h3 className='text-white pb-4'>Improve Your Knowledge</h3>
+                <button className='play-btn btn btn-success'>Let's Play</button>
+            </div>
         </div>
     );
 };
