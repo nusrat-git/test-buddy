08c9f3ba9eaec8c7b5ffbecffd6e8670b329e1b5
nusrat-git <nusratjahanmithila66@gmail.com>
2022-10-11 15:33:17 +0600

    question part done

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -6,12 +6,18 @@ const Questions = ({ questionData }) => {
 
     console.log(questionData);
     return (
-        <div>
-            <h1>{question}</h1>
-            <EyeIcon className="h-6 w-6 text-blue-500"/>
-            <p>
+        <div className=' bg-teal-200 m-8 p-9 rounded-xl'>
+            <div className='flex items-center justify-center gap-4  mt-9'>
+                <h1 className='text-2xl font-semibold'>{question}</h1>
+                <EyeIcon className="h-6 w-6 text-blue-500" />
+            </div>
+            <p className='grid md:grid-cols-2 p-7'>
                 {
-                    options.map(option => <p>{option}</p>)
+                    options.map(option => <div className='flex justify-center items-center'>
+                        <input type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+                        <p className='p-7 text-xl'>{option}</p>
+
+                    </div>)
                 }
             </p>
         </div>
--- src/components/Quizes/Quizes.js
+++ src/components/Quizes/Quizes.js
@@ -4,14 +4,18 @@ import Questions from '../Questions/Questions';
 
 const Quizes = () => {
     const quizData = useLoaderData();
-    const {id , name , total, questions} = quizData.data;
+    const { id, name, total, questions } = quizData.data;
     return (
-        <div>
-            <h1>Test Of {name}</h1>
-            <p>Total Questions: {total}</p>
-            {
-                questions.map(questionData => <Questions key={id} questionData={questionData}></Questions>)
-            }
+        <div className=''>
+            <div className='p-5 m-4'>
+                <h1 className='text-3xl font-bold mb-3'>Test Of {name}</h1>
+                <p className='text-xl mb-3'>Total Questions: {total}</p>
+            </div>
+            <div className=''>
+                {
+                    questions.map(questionData => <Questions key={id} questionData={questionData}></Questions>)
+                }
+            </div>
         </div>
     );
 };
