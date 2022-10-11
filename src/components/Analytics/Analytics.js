ddf97f5f789ba7074bf90825b9c0ae74885113c8
nusrat-git <nusratjahanmithila66@gmail.com>
2022-10-11 18:56:41 +0600

    responsive chart

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { Bar, CartesianGrid, ComposedChart, Legend, Line, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, CartesianGrid, ComposedChart, Legend, Line, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Analytics = () => {
     const quizQuestions = useLoaderData();
@@ -10,25 +10,27 @@ const Analytics = () => {
         <div>
             <h1 className='text-4xl font-bold p-10 text-sky-800'>Chart analysis of total questions</h1>
 
-            <div className='flex justify-center m-6'>
-                <ComposedChart width={500}
-                    height={400}
-                    data={quizTotal}
-                    margin={{
-                        top: 20,
-                        right: 20,
-                        bottom: 20,
-                        left: 20
-                    }}>
-                    <CartesianGrid stroke="#8BBCCC"  ></CartesianGrid>
-                    <XAxis dataKey="name" scale="band" ></XAxis>
-                    <YAxis></YAxis>
-                    <Tooltip></Tooltip>
-                    <Legend></Legend>
-                    <Bar dataKey="total" barSize={20} fill="#BCCEF8" ></Bar>
-                    <Line type="monotone" dataKey="total" stroke="#4C6793"></Line>
+            <div className='flex justify-center mt-6'>
+                {/* <ResponsiveContainer width='100%' height={400} > */}
 
-                </ComposedChart>
+                    <ComposedChart  width={360}
+                        height={400}
+                        data={quizTotal}
+                        margin={{
+                            right:20
+                        }}>
+                        <CartesianGrid stroke="#8BBCCC"  ></CartesianGrid>
+                        <XAxis dataKey="name" scale="band" ></XAxis>
+                        <YAxis></YAxis>
+                        <Tooltip></Tooltip>
+                        <Legend></Legend>
+                        <Bar dataKey="total" barSize={20} fill="#BCCEF8" ></Bar>
+                        <Line type="monotone" dataKey="total" stroke="#4C6793"></Line>
+
+                    </ComposedChart>
+
+                {/* </ResponsiveContainer> */}
+
             </div>
 
         </div>
